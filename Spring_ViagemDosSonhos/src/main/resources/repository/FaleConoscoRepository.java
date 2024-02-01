6585675559ea95e5a3804ca666b666b57356acaf
kikamartinez <140267191+kikamartinez@users.noreply.github.com>
2024-02-01 20:49:07 -0300

    Add files via upload

---
+++ Spring_ViagemDosSonhos/src/main/resources/repository/FaleConoscoRepository.java
@@ -0,0 +1,13 @@
+
+public class FaleConoscoRepository {
+
+}
+package br.com.viagemdossonhos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.viagemdossonhos.model.FaleConosco;
+
+public interface FaleConoscoRepository extends JpaRepository<FaleConosco, Long> {
+
+}
